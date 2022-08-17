6e52ea653063b77afee48b1ce35f9ea373e93ce6
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-17 23:59:00 +0300

    Comment authControllers

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,39 @@ const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/HttpError");
 
-exports.login = async (req, res, next) => {};
-exports.register = async (req, res, next) => {};
-exports.logout = async (req, res, next) => {};
+exports.register = async (req, res, next) => {
+  // Get post data
+  const { email, password } = req.body;
+
+  // Validate input
+  
+
+  // Check if user exists
+  // Hash password
+  // Create new user
+  // Generate JWT
+  // Return new user
+};
+
+exports.login = async (req, res, next) => {
+  // Get post data
+  // Find user
+  // Validate password
+  // Generate JWT
+  // Return user data
+};
+
+exports.refreshToken = async (req, res, next) => {
+  // Get post data
+  // Validate token
+  // Check if token is not in blacklist
+  // Generate new token
+};
+
+exports.logout = async (req, res, next) => {
+  // Get post data
+  // Get expiration date from token
+  // Check if token is in blacklist
+  // Add token to blacklist
+  // Start delete timer
+};
