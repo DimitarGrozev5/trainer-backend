3f432493d7958bd2d5b3a9ba184817e911b91999
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-18 00:02:16 +0300

    add validation to register route

--- src/routers/authRoutes.js
+++ src/routers/authRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 // const { isAuth } = require("../middlewares/authMiddleware");
 const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+router.post(
+  "/register",
+  check("email").normalizeEmail.isEmail(),
+  check("password").isLength({ min: 6 }),
+  authController.register
+);
 router.post("/login", authController.login);
-router.post("/register", authController.register);
 router.post("/logout", authController.logout);
 
 module.exports = router;
