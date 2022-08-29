241a9ecc8637f837006892fb4432eece196bf727
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-29 12:25:17 +0300

    deleteExpiredBlacklistedJWTs implemented

--- src/tasks/deleteExpiredBlacklistedJWTsTask.js
+++ src/tasks/deleteExpiredBlacklistedJWTsTask.js
@@ -1,7 +1,6 @@
-// const BlacklistedJWT = require("../models/BlacklistedJWT");
+const BlacklistedJWT = require("../models/BlacklistedJWT");
 
 exports.deleteExpiredJWTs = async () => {
   const now = +new Date();
-  // TODO: implement with DB
-  // await BlacklistedJWT.deleteMany({ expiresBy: { $lt: now } });
+  await BlacklistedJWT.deleteMany({ expiresBy: { $lt: now } });
 };
