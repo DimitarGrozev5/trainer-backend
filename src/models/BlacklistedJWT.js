df9038cf12c8b8fbcb432fc93c93a000d205753a
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-29 08:08:35 +0300

    created blacklistedJWT Model

---
+++ src/models/BlacklistedJWT.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const blacklistedSchema = new Schema({
+  token: { type: String, required: true, unique: true },
+  expiresBy: { type: Number, required: true },
+});
+blacklistedSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("BlacklistedJWT", blacklistedSchema);
