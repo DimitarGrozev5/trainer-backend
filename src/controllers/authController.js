3074bdf132ba58a6b300e0922422178fb29233c4
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-08-31 08:02:08 +0300

    move rmeove token function to separate file

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/HttpError");
 const User = require("../models/User");
-const BlacklistedJWT = require("../models/BlacklistedJWT");
+const {saveTokenToBlacklist} = require("./helpers/save-token-to-blacklist")
 
 exports.register = async (req, res, next) => {
   // Get post data
@@ -189,58 +189,3 @@ exports.logout = async (req, res, next) => {
     message: "Success",
   });
 };
-
-async function saveTokenToBlacklist(token) {
-  // Check if token is in the blacklist
-  let existingToken;
-  try {
-    existingToken = await BlacklistedJWT.findOne({ token });
-  } catch (err) {
-    console.log(err);
-    const error = new HttpError("Logout failed! Please try again later!", 500);
-    throw error;
-  }
-
-  // Return success
-  if (existingToken) {
-    return true;
-  }
-
-  // Get expiration date from token
-  let expiresBy;
-  try {
-    const decodedToken = jwt.verify(token, process.env.jwtKey);
-    expiresBy = decodedToken.exp * 1000;
-  } catch (err) {
-    const error = new HttpError("Internal server error!", 401);
-    throw error;
-  }
-
-  // Add token to blacklist
-  const newBlacklistToken = new BlacklistedJWT({ token, expiresBy });
-  try {
-    await newBlacklistToken.save();
-  } catch (err) {
-    const error = new HttpError("Logout failed! Please try again later!", 500);
-    throw error;
-  }
-
-  // Start delete timer
-  const timeout = Math.max(0, expiresBy - new Date());
-
-  const deleteTokenAfterTimeout = (timeout) =>
-    setTimeout(async () => {
-      try {
-        // Delete token from blacklist
-        await BlacklistedJWT.findOneAndDelete({ token });
-      } catch (err) {
-        // Start another timer to try the deletion again
-        deleteTokenAfterTimeout(10 * 60 * 1000);
-        console.log(err);
-      }
-    }, timeout);
-
-  deleteTokenAfterTimeout(timeout);
-
-  return true;
-}
---
+++ src/controllers/helpers/save-token-to-blacklist.js
@@ -0,0 +1,59 @@
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../../models/HttpError");
+const BlacklistedJWT = require("../../models/BlacklistedJWT");
+
+exports.saveTokenToBlacklist = async (token) => {
+  // Check if token is in the blacklist
+  let existingToken;
+  try {
+    existingToken = await BlacklistedJWT.findOne({ token });
+  } catch (err) {
+    console.log(err);
+    const error = new HttpError("Logout failed! Please try again later!", 500);
+    throw error;
+  }
+
+  // Return success
+  if (existingToken) {
+    return true;
+  }
+
+  // Get expiration date from token
+  let expiresBy;
+  try {
+    const decodedToken = jwt.verify(token, process.env.jwtKey);
+    expiresBy = decodedToken.exp * 1000;
+  } catch (err) {
+    const error = new HttpError("Internal server error!", 401);
+    throw error;
+  }
+
+  // Add token to blacklist
+  const newBlacklistToken = new BlacklistedJWT({ token, expiresBy });
+  try {
+    await newBlacklistToken.save();
+  } catch (err) {
+    const error = new HttpError("Logout failed! Please try again later!", 500);
+    throw error;
+  }
+
+  // Start delete timer
+  const timeout = Math.max(0, expiresBy - new Date());
+
+  const deleteTokenAfterTimeout = (timeout) =>
+    setTimeout(async () => {
+      try {
+        // Delete token from blacklist
+        await BlacklistedJWT.findOneAndDelete({ token });
+      } catch (err) {
+        // Start another timer to try the deletion again
+        deleteTokenAfterTimeout(10 * 60 * 1000);
+        console.log(err);
+      }
+    }, timeout);
+
+  deleteTokenAfterTimeout(timeout);
+
+  return true;
+};
