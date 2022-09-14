70ebb78b801eb8b118e3828d317716acaf2bd0af
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-14 17:12:41 +0300

    update ees getNextState

--- src/programs/ees/even-easier-strength.js
+++ src/programs/ees/even-easier-strength.js
@@ -11,15 +11,19 @@ module.exports = {
     };
   },
 
-  getNextState: () => {
+  getNextState: (
+    state,
+    achieved,
+    { forceProgress = false } = {
+      forceProgress: false,
+    }
+  ) => {
     return {
-      setsDone: {
-        push: 0,
-        pull: 0,
-        squat: 0,
-        ab: 0,
-        accessory: 0,
-      },
+      push: achieved.push % 2,
+      pull: achieved.pull % 2,
+      squat: achieved.squat % 2,
+      ab: achieved.ab % 2,
+      accessory: achieved.accessory % 2,
     };
   },
 };
