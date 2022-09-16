193dff3a1013df22de0ae705e93b831004f5dbcd
DimitarGrozev5 <dimgrozev5@gmail.com>
2022-09-16 17:33:15 +0300

    comment

--- src/services/program-transformer.js
+++ src/services/program-transformer.js
@@ -1,10 +1,13 @@
 import bcrypt from 'bcrypt';
 
 export const fromModel = async (id, state, salt) => {
+  // Generate sting based on program state
   const hashableData = JSON.stringify({ id, state });
 
+  // Get the bcrypt output
   const versionFullHash = await bcrypt.hash(hashableData, salt);
 
+  // Extract only the hash
   const versionHash = versionFullHash.split('$').pop().substring(22);
 
   return {
