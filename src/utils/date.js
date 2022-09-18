e1c481bf0a0c928c220c37b5d8af0007cdec6d27
DimitarGrozev5 <95020107+DimitarGrozev5@users.noreply.github.com>
2022-09-18 15:21:29 +0300

    save tsc result

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,42 +1,42 @@
-import { add } from "date-fns";
+import { last } from './array';
 // Get month name
 export const getMonthName = (date) => {
     return [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December',
     ][date.getMonth()];
 };
-export const getWeekDayShortName = (day) => ["Mon", "Thu", "Thi", "Wed", "Fri", "Sat", "Sun"][day % 7];
+export const getWeekDayShortName = (day) => ['Mon', 'Thu', 'Thi', 'Wed', 'Fri', 'Sat', 'Sun'][day % 7];
 // Function to add leading zeroes
 export const lz = (num, length = 2) => {
     return (100000 + num).toString().substring(6 - length);
 };
 // Remove hours minutes and seconds from Date
-export const roundDate = (date, to = "hours") => {
+export const roundDate = (date, to = 'hours') => {
     const d = new Date(date);
     switch (to) {
         // @ts-ignore
-        case "hours": // eslint-disable-line
-            d.setHours(0);
+        case 'hours': // eslint-disable-line
+            d.setUTCHours(0);
         // @ts-ignore
-        case "minutes": // eslint-disable-line
-            d.setMinutes(0);
+        case 'minutes': // eslint-disable-line
+            d.setUTCMinutes(0);
         // @ts-ignore
-        case "seconds": // eslint-disable-line
-            d.setSeconds(0);
+        case 'seconds': // eslint-disable-line
+            d.setUTCSeconds(0);
         // @ts-ignore
-        case "miliseconds": // eslint-disable-line
-            d.setMilliseconds(0);
+        case 'miliseconds': // eslint-disable-line
+            d.setUTCMilliseconds(0);
         default: // eslint-disable-line
             break;
     }
@@ -55,22 +55,7 @@ export const sameMonth = (date1, date2) => {
     return (date1.getFullYear() === date2.getFullYear() &&
         date1.getMonth() === date2.getMonth());
 };
-const nextDaysMonth = (arr) => {
-    const last = arr.length;
-    if (!last) {
-        return -1;
-    }
-    const lastValLen = arr[last - 1].length;
-    if (!lastValLen) {
-        return -1;
-    }
-    const today = arr[last - 1][lastValLen - 1];
-    const nextDay = add(today, { days: 1 });
-    return today.getMonth() === 11 && nextDay.getMonth() === 0
-        ? 12
-        : nextDay.getMonth();
-};
-export const getMonthArr = (date, { getNumOfWeeks = 6, skipDaysFromOtherMonths = false } = {}) => {
+export const getMonthArr = (date, { getNumOfWeeks = 6 } = {}) => {
     // Get current month
     const currentMonth = date.getMonth();
     // Get first day of month
@@ -86,7 +71,10 @@ export const getMonthArr = (date, { getNumOfWeeks = 6, skipDaysFromOtherMonths =
     const mondayDateUTC = firstDay.getTime() - firstDayOfWeek * 24 * 60 * 60 * 1000;
     // Generate array for month
     const month = [];
-    while (month.length < getNumOfWeeks || nextDaysMonth(month) <= currentMonth) {
+    let lastDayOfWeek = firstDay;
+    // Loop through weeks until reaching the desired number of weeks or until the date goes to another month
+    while (month.length < getNumOfWeeks ||
+        lastDayOfWeek.getMonth() === currentMonth) {
         const weekArr = [];
         for (let day = 0; day < 7; day++) {
             const today = new Date(mondayDateUTC +
@@ -95,6 +83,7 @@ export const getMonthArr = (date, { getNumOfWeeks = 6, skipDaysFromOtherMonths =
             weekArr.push(today);
         }
         month.push(weekArr);
+        lastDayOfWeek = last(weekArr);
     }
     return month;
 };
